57ce8fe9eceae213b7a94ab3b70d1cfd9240b9a0
sidharth <sidhartharelhan@gmail.com>
2014-05-18 22:05:10 +0530

    navigation

---
+++ navigation/js/app.js
@@ -0,0 +1,18 @@
+angular.module('airline', [])
+	.config(airlineRouter);
+
+function airlineRouter ($routeProvider) {
+	$routeProvider
+		.when('/', {templateUrl: 'partials/destinations.html',
+		 controller: function  ($scope) {
+		 	$scope.setActive('destinations');
+		 }})
+		.when('/flights', {template: '<h3>Flights</h3>',
+		 controller: function  ($scope) {
+		 	$scope.setActive('flights');
+		 }})
+		.when('/reservations', {template: '<h3>Your Reservations</h3>',
+		 controller: function  ($scope) {
+		 	$scope.setActive('reservations');
+		 }});
+}
